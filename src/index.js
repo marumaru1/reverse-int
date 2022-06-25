35d101ffe90a36ccd06b8a960c0f1236ffcea647
Maria Makliuk <annamary_1995@mail.ru>
2022-06-25 15:50:36 +0300

    feat:do that

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let reverse = n.toString().split('').reverse().join('');
+    if (reverse.endsWith('-')) {
+      reverse = '' + reverse;
+    }
+    
+    reverse = parseInt(reverse);
+    return reverse
 }
